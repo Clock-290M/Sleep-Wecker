2949ee791112bff1e78aa3f62aad38160cee4b03
Mjd4686 <dowmark4686@gmail.com>
2018-04-26 21:54:55 -0400

    Please enter the commit message for your changes. Lines starting

--- software/arduino/test_bed/test_bed.ino
+++ software/arduino/test_bed/test_bed.ino
@@ -1,43 +1,103 @@
 #include <ESP8266WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#include <time.h>
 
-const int REEDPIN = D2;
-bool reeddetected = false;
-bool alarmon = false;
-int alarmhours = 7;
-int alarmminutes = 30;
+const int REEDPIN = D5;
+const int NEOPIXELPIN = D3;
+bool alarmdisabled = false;
+int alarmhours = 23;
+int alarmminutes = 18;
 int currhours = 0;
 int currminutes = 0;
-
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, NEOPIXELPIN, NEO_GRB + NEO_KHZ800);
+String ssid = "iPhone";
+String password = "mark1234";
 void setup() {
   Serial.begin(9600);
   pinMode(REEDPIN, INPUT_PULLUP);
+  ring.begin();
+  ring.clear();
+  ring.show();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("iPhone", "mark1234");
+  Serial.println("\nConnecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+  Serial.print(".");
+  delay(1000);
+  }
+  configTime((-4) * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("\nWaiting for time");
+  while (!time(nullptr)) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
 }
 
-void playNoise() {
-  
+bool timecompare(int a, int b, int c, int d){
+  int temp1 = a*100;
+  temp1 = temp1 + b;
+  int temp2 = c*100;
+  temp2 = temp2 + d;
+  if(temp1 >= temp2){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 void loop() {
+
+  //update time here
+    time_t now = time(nullptr);
+    Serial.println(ctime(&now));
+    
+    int temp = 0;
+    if(ctime(&now)[11] != 0){
+      currhours = (ctime(&now)[11] - '0');
+      currhours = currhours*10;
+    }
+    currhours = currhours + (ctime(&now)[12] - '0');
+    //Serial.println(currhours);
+    temp = 0;
+    if(ctime(&now)[14] != 0){
+      currminutes = (ctime(&now)[14] - '0');
+      currminutes = currminutes*10;
+    }
+    currminutes = currminutes + (ctime(&now)[15] - '0');
+    //Serial.println(currminutes);
+    
+    //Serial.println(hour(&now);
+    delay(1000);
+  
+  while((timecompare(currhours, currminutes, alarmhours, alarmminutes)==true) && (alarmdisabled == false)){
+    //play sound
+    //light up neopixel
+    ring.clear();
+    int color[] = {255, 0 ,0};
+    for(int i = 0; i < 16; i++){
+      ring.setPixelColor(i, 255, 0, 0);
+    }
+    ring.show();
+    Serial.println("ALARM");
+    
+    if(digitalRead(REEDPIN) == LOW){
+      alarmdisabled = true;
+    }
+    
+    delay(10);
+  }
+    ring.clear();
+    for(int i = 0; i < 16; i++){
+      ring.setPixelColor(i, 0,0,0);
+    }
+     ring.show();
   //Serial.println(digitalRead(REEDPIN));
-  if(digitalRead(REEDPIN) == LOW){
-    reeddetected = true;
-  }
-  else{
-    reeddetected = false;
-  }
-  //update time here 
-  alarm();
-  if(alarmon && !reeddetected){
-    //do annoying stuff;
-  }
-  else if(alarmon && reeddetected){
-    //disablealarm
-  }
 }
-void alarm(){
-  if((currhours >= alarmhours) && (currminutes >= alarmminutes)){
-    alarmon = true;
-  }
-}
+
+
+
 
 
